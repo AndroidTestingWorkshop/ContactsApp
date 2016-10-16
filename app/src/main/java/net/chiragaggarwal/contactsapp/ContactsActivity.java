d9ede34797d734f6bab5b33cb9dd3cdea5423b3c
Chirag <me@chi6rag.net>
2016-10-16 16:28:01 +0530

    Adding add contact screen

--- app/src/main/java/net/chiragaggarwal/contactsapp/ContactsActivity.java
+++ app/src/main/java/net/chiragaggarwal/contactsapp/ContactsActivity.java
@@ -2,9 +2,11 @@ package net.chiragaggarwal.contactsapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.List;
 
@@ -25,12 +27,20 @@ public class ContactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contacts);
 
         listContacts = (RecyclerView) findViewById(R.id.list_contacts);
+        FloatingActionButton fabAddContact = (FloatingActionButton) findViewById(R.id.fab_add_contact);
         listContacts.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         contactsAdapter = new ContactsAdapter(this, new OnContactClickListener() {
             @Override
             public void onContactClick(int contactId) {
                 Intent contactIntent = new Intent(ContactsActivity.this, ContactActivity.class);
                 contactIntent.putExtra(Contact.ID, contactId);
+                startActivity(contactIntent);
+            }
+        });
+        fabAddContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactIntent = new Intent(ContactsActivity.this, AddContactActivity.class);
                 startActivity(contactIntent);
             }
         });
