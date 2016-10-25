09dc84d5a8f16df16974791d3eb873d2814cbe78
Chirag <me@chi6rag.net>
2016-10-25 13:13:43 +0530

    Preparing for refactoring

--- app/src/main/java/net/chiragaggarwal/contactsapp/AddContactActivity.java
+++ app/src/main/java/net/chiragaggarwal/contactsapp/AddContactActivity.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.HttpException;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Subscriber;
@@ -31,16 +34,61 @@ public class AddContactActivity extends AppCompatActivity {
         inputLastName = (EditText) findViewById(R.id.input_last_name);
         inputEmail = (EditText) findViewById(R.id.input_email);
         inputPhone = (EditText) findViewById(R.id.input_phone);
-        addContactDetailTextChangedListeners(buttonAddContact, inputFirstName, inputEmail, inputPhone, inputLastName);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(getString(R.string.base_url))
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
         final ContactsNetworkService contactsNetworkService = retrofit.create(ContactsNetworkService.class);
+
         buttonAddContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (contentof(inputFirstName).length() < 2) {
+                    new AlertDialog.Builder(AddContactActivity.this)
+                            .setMessage(R.string.error_invalid_name)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            }).create().show();
+                    return;
+                }
+
+                if (contentof(inputLastName).length() < 2) {
+                    new AlertDialog.Builder(AddContactActivity.this)
+                            .setMessage(R.string.error_invalid_name)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            }).create().show();
+                    return;
+                }
+
+                if (inputPhone.getText().toString().isEmpty()) {
+                    new AlertDialog.Builder(AddContactActivity.this)
+                            .setMessage(R.string.error_invalid_phone)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            }).create().show();
+                    return;
+                }
+
+                if (!contentof(inputEmail).contains("@")) {
+                    new AlertDialog.Builder(AddContactActivity.this)
+                            .setMessage(R.string.error_invalid_email)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            }).create().show();
+                    return;
+                }
+
                 Contact contact = new Contact(contentof(inputFirstName), contentof(inputLastName), contentof(inputEmail), contentof(inputPhone));
                 contactsNetworkService.createContact(contact)
                         .subscribeOn(Schedulers.io())
@@ -48,52 +96,31 @@ public class AddContactActivity extends AppCompatActivity {
                         .subscribe(new Subscriber<Contact>() {
                             @Override
                             public void onCompleted() {
-
+                                System.out.println();
                             }
 
                             @Override
                             public void onError(Throwable e) {
+                                try {
+                                    new AlertDialog.Builder(AddContactActivity.this)
+                                            .setMessage(((HttpException) e).response().errorBody().string())
+                                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                }
+                                            }).create().show();
+                                } catch (IOException e1) {
+                                    e1.printStackTrace();
+                                }
                             }
 
                             @Override
                             public void onNext(Contact contact) {
-                                new AlertDialog.Builder(AddContactActivity.this)
-                                        .setMessage(R.string.contact_created)
-                                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                finish();
-                                            }
-                                        }).create().show();
+                                finish();
                             }
                         });
             }
         });
-    }
-
-    private void addContactDetailTextChangedListeners(final TextView... textViews) {
-        for (TextView textView : textViews) {
-            textView.addTextChangedListener(new ContactDetailTextChangeWatcher() {
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    toggleAddContactButtonState(buttonAddContact, inputFirstName, inputLastName, inputEmail, inputPhone);
-                }
-            });
-        }
-    }
-
-    private void toggleAddContactButtonState(TextView buttonAddContact, EditText inputFirstName, EditText inputLastName, EditText inputEmail, EditText inputPhone) {
-        boolean areRequiredFieldsPresent = isPresent(inputFirstName) && isPresent(inputLastName) && isPresent(inputEmail) && isPresent(inputPhone);
-        buttonAddContact.setBackgroundColor(areRequiredFieldsPresent ? getResources().getColor(R.color.add_contact_green) : getResources().getColor(R.color.light_gray));
-        buttonAddContact.setEnabled(areRequiredFieldsPresent);
-    }
-
-    private boolean isPresent(EditText input) {
-        return !isNotPresent(input);
-    }
-
-    private boolean isNotPresent(EditText input) {
-        return input.getText().toString().length() < 2;
     }
 
     private String contentof(TextView textView) {
